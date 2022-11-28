6cb2e17c424c92770752113ae6ecf803c30ce474
JI Villalobos <chuy12villalobos@gmail.com>
2022-11-28 16:14:09 -0600

    add-- header on new client page

--- pages/new.tsx
+++ pages/new.tsx
@@ -1,9 +1,13 @@
+import Header from "./components/Header";
 import NewClientForm from "./components/NewClientForm";
 
 export default function NewClient() {
-    return(
-        <div className="flex items-center justify-center mt-20">
-            <NewClientForm />
-        </div>
+    return (
+        <>
+            <Header />
+            <div className="flex items-center justify-center mt-20">
+                <NewClientForm />
+            </div>
+        </>
     )
 }
