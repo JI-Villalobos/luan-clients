084b48abbfe423a519b737f0d7b3970239fa8281
JI Villalobos <chuy12villalobos@gmail.com>
2022-11-28 09:37:54 -0600

    delete-- portal

--- pages/components/Header.tsx
+++ pages/components/Header.tsx
@@ -11,7 +11,10 @@ export default function Header() {
                             </a>
                         </div>
                         <div className="-my-2 -mr-2 md:hidden">
-                            <button type="button" className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" aria-expanded="false">
+                            <button 
+                                type="button" 
+                                className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" aria-expanded="false"
+                                >
                                 <span className="sr-only">Open menu</span>
                             </button>
                         </div>
